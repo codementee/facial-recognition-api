f21aaa6e8e3657d491dd1f7de93073f209f0440d
codementee <codementee@outlook.com>
2020-05-16 20:31:15 +0500

    connected server to the regester page

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ app.post('/register', (req, res) => {
             id: '124',
             name: name, 
             email: email,
-            password: password,
             entries: 0,
             joined: new Date()  
     })
