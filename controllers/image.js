cac625199cc369ea0b1d572d20a014bf5104df01
codementee <codementee@outlook.com>
2020-07-04 02:26:47 +0500

    update

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '90ee53e5901f478ba513e2ecf69ea73c'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const {id} = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +24,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res)})
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
 app.listen(3000, () => {
     console.log('app is running on 3000')
 });
