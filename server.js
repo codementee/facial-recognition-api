c61484909efff36f3bc29dadc5badb6d11afde5f
codementee <codementee@outlook.com>
2020-07-06 01:43:18 +0500

    updating database

--- server.js
+++ server.js
@@ -14,11 +14,9 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'dbpass',
-      database : 'fcrec'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
   });
 
