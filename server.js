3720775baee52ea5bf42ce0828eafcedabb50d36
codementee <codementee@outlook.com>
2020-07-07 01:22:51 +0500

    error handled

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const knex = require('knex');
 const pg = require('pg');
 const { response } = require('express');
 
-const register = require('./cont  rollers/register');
+const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/get-profile');
 const image = require('./controllers/image');
