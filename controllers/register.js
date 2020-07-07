0400c0600e3453e79690262fb29521ab8d3795ea
codementee <codementee@outlook.com>
2020-07-07 19:26:30 +0500

    update

--- controllers/register.js
+++ controllers/register.js
@@ -3,11 +3,12 @@ const pg = require('pg');
 
 const db = knex({
     client: 'pg',
+    connection: {
       connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
-    
+    }
   });
 
 
