ffa67d80f185aac0ec87061c033bdaf48d60b505
codementee <codementee@outlook.com>
2020-07-07 20:07:34 +0500

    update

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/get-profile');
 const image = require('./controllers/image');
 
-
+const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
 // const db = knex({
 //   client: 'pg',
 //   connection: {
